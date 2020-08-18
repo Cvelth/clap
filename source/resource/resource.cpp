622864cd7c7f978fa21c85074131d62ee4b579b4
cvelth <cvelth.mail@gmail.com>
2020-08-18 17:43:46 +0300

    Merge 'load_texture.cpp' into 'resource.cpp'.

--- source/resource/resource.cpp
+++ source/resource/resource.cpp
@@ -147,3 +147,21 @@ void clap::resource::clear() {
 void clap::resource::detail::load_shader(shaders_t &storage, std::string const &name, gl::shader::detail::object *object) {
 	storage.insert(std::pair(name, object));
 }
+
+#include "lodepng/lodepng.h"
+void clap::resource::detail::load_texture(std::string const &filename, std::string const &texture_name) {
+	std::vector<unsigned char> image_data;
+	unsigned width, height;
+	unsigned error_code = lodepng::decode(image_data, width, height, filename);
+	if (error_code != 0) {
+		log::warning::major << "Error while trying to decode a texture file.";
+		log::info::critical << "Error code: " << error_code << '.';
+		log::info::critical << lodepng_error_text(error_code);
+		return;
+	} else {
+		log::message::minor << "A texture (" << image_data.size() << " bytes) was loaded successfully.";
+		log::info::major << "Path: '" << filename << "'.";
+	}
+
+	texture::_2d.insert(std::pair(texture_name, new gl::texture::_2d(image_data.data(), width, height)));
+}
