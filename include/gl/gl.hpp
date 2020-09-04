e4a10bfce2b176eaedc524cad54efe4fef0d1d0e
cvelth <cvelth.mail@gmail.com>
2020-09-04 10:56:29 +0300

    Add 'gl/texture.hpp' include to 'gl/gl.hpp'

--- include/gl/gl.hpp
+++ include/gl/gl.hpp
@@ -2,6 +2,7 @@
 #include "gl/buffer.hpp"
 #include "gl/misc.hpp"
 #include "gl/shader.hpp"
+#include "gl/texture.hpp"
 #include "gl/vertex_array.hpp"
 
 #define FORCE_NVIDIA_GPU_ON_OPTIMUS extern "C" { \
