fb211840b346688f1417843928ee9915b999b74e
cvelth <cvelth.mail@gmail.com>
2020-09-05 14:46:16 +0300

    Fix 'fragment/text' shader.
    Arguments may not be named 'texture' under NVidia GPU.
    Because of that, sampler uniform was renamed to 'font_atlas'.

--- example/text/resource/shader/fragment/text.glsl
+++ example/text/resource/shader/fragment/text.glsl
@@ -4,10 +4,10 @@ in vec2 fragment_texture_coordinates;
 
 out vec4 color_output;
 
-uniform sampler2D texture;
+uniform sampler2D font_atlas;
 
 void main() {
     color_output = vec4(1, 1, 1, 
-        texture(texture, fragment_texture_coordinates).r
+        texture(font_atlas, fragment_texture_coordinates).r
     );
 }
