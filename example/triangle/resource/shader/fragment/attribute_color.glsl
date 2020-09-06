6dd13d0bec0785becfa989d220152c3f5e7e86f4
cvelth <cvelth.mail@gmail.com>
2020-09-06 23:11:14 +0300

    Combine 'triangle' and 'texture' examples into one.

--- example/triangle/resource/shader/fragment/attribute_color.glsl
+++
@@ -1,9 +0,0 @@
-#version 330 core
-
-in vec4 fragment_color;
-
-out vec4 color_output;
-
-void main() {
-    color_output = fragment_color;
-} 
--- example/triangle/window.cpp
+++ example/triangle/window.cpp
@@ -4,9 +4,6 @@
 #include <chrono>
 #include <thread>
 
-//#include <iostream> 
-//#include "../../dependencies/include/glad/glad.h"
-
 //FORCE_NVIDIA_GPU_ON_OPTIMUS;
 
 void window::initialize() {
@@ -15,42 +12,61 @@ void window::initialize() {
 	using namespace clap::gl;
 	clear::set_color(0.4f, 0.0f, 0.5f, 1.0f);
 
+	auto &texture = clap::resource::texture::_2d["logo"];
+	texture.set_min_filter(texture::min_filter::linear_mipmap_linear);
+	texture.set_mag_filter(texture::mag_filter::linear);
+	texture.bind();
 	
-	program.add(clap::resource::shader::fragment["attribute_color"], 
-				clap::resource::shader::vertex["position_color"]);
+	program.add(clap::resource::shader::fragment["triangle"], 
+				clap::resource::shader::vertex["triangle"]);
 	program.link();
 
-	//auto uniforms = program.getUniforms();
+	uniforms = program.getUniforms();
 	auto attributes = program.getAttributes();
 
 	buffer::single vertices;
 	static const float vertex_data[] = {
-		-1.0f, -1.0f, 0.0f,
-		1.0f, -1.0f, 0.0f,
-		0.0f,  1.0f, 0.0f,
+		-1.0f, -1.0f,
+		1.0f, -1.0f, 
+		0.0f,  1.0f, 
 	};
 	vertices.data(vertex_data, sizeof(vertex_data), buffer::usage::static_draw);
-	vertex_array.attribute_pointer(vertices, attributes["position"], 0, 0);
+	vertex_array.attribute_pointer(vertices, attributes["position"]);
 
 	buffer::single colors;
 	static const float color_data[] = {
-		1.f, 0.5f, 0.5f, 1.f,
-		0.5f, 1.f, 0.5f, 1.f,
-		0.5f, 0.5f, 1.f, 1.f,
+		1.f, 0.5f, 0.5f,
+		0.5f, 1.f, 0.5f,
+		0.5f, 0.5f, 1.f,
 	};
 	colors.data(color_data, sizeof(color_data), buffer::usage::static_draw);
-	vertex_array.attribute_pointer(colors, attributes["color"], 0, 0);
+	vertex_array.attribute_pointer(colors, attributes["color"]);
 
+	buffer::single texture_coordinates;
+	static const float texture_coordinate_data[] = {
+		0.f,  1.f,
+		1.f,  1.f,
+		0.5f, 0.f
+	};
+	texture_coordinates.data(texture_coordinate_data, sizeof(texture_coordinate_data), buffer::usage::static_draw);
+	vertex_array.attribute_pointer(texture_coordinates, attributes["texture_coordinates"]);
+
+	mode = 0;
 	program.use();
+	program.set(uniforms["mode"], { mode });
+	update();
 }
 
 void window::render() {
 	using namespace clap::gl;
-	auto time = std::chrono::high_resolution_clock::now() + std::chrono::milliseconds(100);
+	auto time = std::chrono::high_resolution_clock::now() + std::chrono::milliseconds(10);
 
 	poll_events();
-	clear::color();
-	vertex_array.draw(vertex_array::connection::triangles, 3);
+	if (should_update()) {
+		clear::color();
+		vertex_array.draw(vertex_array::connection::triangles, 3);
+		redraw();
+	}
 
 	std::this_thread::sleep_until(time);
 }
@@ -59,4 +75,20 @@ void window::cleanup() {
 	using namespace clap::gl;
 
 	clap::resource::clear();
+}
+
+bool window::on_mouse_button(clap::event::mouse_button button, clap::event::mouse_button_action action, clap::event::modificator_mask modificators) {
+	static unsigned old_mode = 0;
+	if (action == clap::event::mouse_button_action::press) {
+		old_mode = !mode;
+		mode = 2;
+	} else if (action == clap::event::mouse_button_action::release) {
+		mode = old_mode;
+		program.set(uniforms["mode"], { mode });
+	}
+	update();
+
+	button; action; modificators; // To avoid "Unreferenced formal parameter" warning.
+	
+	return true;
 }
