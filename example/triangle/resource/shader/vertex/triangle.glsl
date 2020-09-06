26e6c2300f6544faee9cb5c5b4b2d93abac904bb
Cvelth <Cvelth.mail@gmail.com>
2020-09-06 23:14:49 +0300

    Combine 'triangle' and 'texture' examples into 'triangle'. (#11)

--- example/triangle/resource/shader/vertex/triangle.glsl
+++ example/triangle/resource/shader/vertex/triangle.glsl
@@ -1,11 +1,14 @@
 #version 330 core
 
-in vec3 position;
+in vec2 position;
+in vec3 color;
 in vec2 texture_coordinates;
 
+out vec3 fragment_color;
 out vec2 fragment_texture_coordinates;
 
 void main() {
-    gl_Position = vec4(position, 1.0);
+    gl_Position = vec4(position, 0.0, 1.0);
+    fragment_color = color;
     fragment_texture_coordinates = texture_coordinates;
 }
--- example/triangle/window.hpp
+++ example/triangle/window.hpp
@@ -13,7 +13,14 @@ protected:
 	virtual void render() override;
 	virtual void cleanup() override;
 
+	virtual bool on_mouse_button(clap::event::mouse_button button, 
+								 clap::event::mouse_button_action action,
+								 clap::event::modificator_mask modificators) override;
+
 private:
 	clap::gl::vertex_array::single vertex_array;
 	clap::gl::shader::program program;
+	clap::gl::shader::variables uniforms;
+
+	unsigned mode;
 };
