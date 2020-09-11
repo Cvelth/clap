aa79e572a0970afb852aab938c02edf64108767c
cvelth <cvelth.mail@gmail.com>
2020-09-11 14:10:25 +0300

    Define 'pile' implementation structure based on 'compound::list' template.

--- include/ui/pile.hpp
+++ include/ui/pile.hpp
@@ -1,5 +1,39 @@
 ﻿#pragma once
-#include "ui/zone.hpp"
+#include "ui/compound.hpp"
+
+#include <variant>
+
 namespace clap::ui {
-	class pile : public zone {};
+	class pile;
+	namespace detail {
+		struct absolute_pile_constraints {
+			size_t x, y, w, h;
+		};
+		struct relative_pile_constraints {
+			float x, y, w, h;
+		};
+		void verify_relative_constraints(float x, float y, float w, float h);
+
+		class pile_constraints {
+			friend pile;
+		public:
+			void absolute(size_t x, size_t y, size_t w, size_t h) noexcept {
+				value = absolute_pile_constraints{ x, y, w, h };
+			}
+			void relative(float x, float y, float w, float h) noexcept {
+				verify_relative_constraints(x, y, w, h);
+				value = relative_pile_constraints{ x, y, w, h };
+			}
+		protected:
+			std::variant<
+				absolute_pile_constraints,
+				relative_pile_constraints
+			> value;
+		};
+	}
+
+	class pile : public compound::list<detail::pile_constraints> {
+	public:
+		using compound::list<detail::pile_constraints>::list;
+	};
 }
--- source/ui/pile.cpp
+++ source/ui/pile.cpp
@@ -1,1 +1,17 @@
 ﻿#include "ui/pile.hpp"
+
+#include "essential/log.hpp"
+
+void verify_relative_constraint(float v) {
+	if (v <= 0.f || v >= 1.f) {
+		clap::log::warning::critical << "Relative constraint in unacceptable. Value must be in range (0.f, 1.f).";
+		clap::log::info::major << "Value: " << v << ".";
+	}
+}
+
+void clap::ui::detail::verify_relative_constraints(float x, float y, float w, float h) {
+	verify_relative_constraint(x);
+	verify_relative_constraint(y);
+	verify_relative_constraint(w);
+	verify_relative_constraint(h);
+}
