913db22fe9adc119c6d89eb086899287aea791c3
cvelth <cvelth.mail@gmail.com>
2020-09-11 14:09:49 +0300

    Add 'compound' namespace and its members: - 'interface'; - 'list'; - 'set'; - 'vector'.

---
+++ include/ui/compound.hpp
@@ -0,0 +1,52 @@
+﻿#pragma once
+#include "ui/zone.hpp"
+
+#include <list>
+#include <memory>
+#include <set>
+#include <vector>
+
+namespace clap::ui::compound {
+	class interface : public zone {
+	public:
+		using zone::zone;
+	};
+
+	namespace detail {
+		template <typename size_preference_t>
+		struct stored_item {
+			std::shared_ptr<zone> item;
+			size_preference_t size;
+
+			template <typename ...Ts>
+			stored_item(compound::interface *ptr, Ts const &...ts)
+				: item(std::make_shared<zone>(ptr, std::forward<Ts>(ts)...)),
+				size() {}
+		};
+	}
+
+	template <typename size_preference_t>
+	class list : public interface, protected std::list<detail::stored_item<size_preference_t>> {
+		using underlying_t = std::list<detail::stored_item<size_preference_t>>;
+	public:
+		using interface::interface;
+
+		template <typename type, typename ...Ts>
+		size_preference_t& emplace_back(Ts const &...ts) {
+			underlying_t::emplace_back(static_cast<interface *>(this), std::forward<Ts>(ts)...);
+			return underlying_t::back().size;
+		}
+	};
+
+	template <typename size_preference_t>
+	class set : public interface, protected std::set<detail::stored_item<size_preference_t>> {
+	public:
+		using interface::interface;
+	};
+
+	template <typename size_preference_t>
+	class vector : public interface, protected std::vector<detail::stored_item<size_preference_t>> {
+	public:
+		using interface::interface;
+	};
+}
---
+++ source/ui/compound.cpp
@@ -0,0 +1,1 @@
+﻿#include "ui/pile.hpp"
