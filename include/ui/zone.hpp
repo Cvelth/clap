83de394fd2290bbb7a68cc2a756dc075d945b725
cvelth <cvelth.mail@gmail.com>
2020-09-11 14:08:26 +0300

    Define 'zone' costruction, size_constraits and owners.

--- include/ui/zone.hpp
+++ include/ui/zone.hpp
@@ -1,5 +1,42 @@
 ﻿#pragma once
 #include "ui/entity.hpp"
+
+#include <memory>
+#include <string>
+
 namespace clap::ui {
-	class zone : public entity {};
+	class zone;
+	namespace detail {
+		enum class resize_preference {
+			fixed_minimum, prefer_minimum, free, prefer_maximum, fixed_maximum
+		};
+		struct size_constraits {
+			friend zone;
+		public:
+			resize_preference width, height;
+			size_t maximum_w, maximum_h;
+			size_t minimum_w, minimum_h;
+		protected:
+			size_t current_x, current_y,
+				current_w, current_h;
+
+			size_constraits() noexcept;
+			size_constraits(size_t width, size_t height) noexcept;
+		};
+	}
+	namespace compound {
+		class interface;
+	}
+
+	class zone : public entity {
+	public:
+		explicit zone(compound::interface *owner) noexcept;
+		zone(std::u8string name, size_t width, size_t height) noexcept;
+
+	protected:
+		std::u8string name;
+		compound::interface *owner;
+
+		detail::size_constraits size;
+	};
 }
--- source/ui/zone.cpp
+++ source/ui/zone.cpp
@@ -1,1 +1,14 @@
 ﻿#include "ui/zone.hpp"
+
+clap::ui::zone::zone(std::u8string name, size_t width, size_t height) noexcept 
+	: name(name), owner(nullptr), size(width, height) {}
+clap::ui::zone::zone(clap::ui::compound::interface *owner) noexcept
+	: owner(owner) {}
+
+clap::ui::detail::size_constraits::size_constraits() noexcept : size_constraits(0u, 0u) {}
+
+clap::ui::detail::size_constraits::size_constraits(size_t width, size_t height) noexcept :
+	width(resize_preference::prefer_maximum), height(resize_preference::prefer_maximum),
+	maximum_w(std::numeric_limits<size_t>::max()), maximum_h(std::numeric_limits<size_t>::max()),
+	minimum_w(std::numeric_limits<size_t>::min()), minimum_h(std::numeric_limits<size_t>::min()),
+	current_x(0u), current_y(0u), current_w(width), current_h(height) {}
