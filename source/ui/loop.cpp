e46fe4b2570ad04e4cb30a5f3d7c0df1d643d84e
cvelth <cvelth.mail@gmail.com>
2020-09-12 20:45:28 +0300

    Implement multi-threaded event loop. Main thread is only focused on adding/removing worker threads (on window creation/deletion) and events, where as window initialization, drawing and cleaning up is done on separate threads, managed inside 'ui::loop'.

--- source/ui/loop.cpp
+++ source/ui/loop.cpp
@@ -7,32 +7,62 @@
 
 #include "nowide/args.hpp"
 
+#include "essential/guard.hpp"
 #include "essential/log.hpp"
 
+static constexpr auto iteration_interval = std::chrono::milliseconds(size_t(1000.f / 30));
+
 int clap::ui::loop(int argc, char **argv) {
-    nowide::args _(argc, argv);
-    log::message::minor << "Passed arguments (" << argc << "):";
-    for (int i = 0; i < argc; i++)
-        log::info::critical << i << ": " << argv[i] << ".";
+	nowide::args _(argc, argv);
+	log::message::minor << "Passed arguments (" << argc << "):";
+	for (int i = 0; i < argc; i++)
+		log::info::critical << i << ": " << argv[i] << ".";
 
-    //Initialization
-    
-    log::message::major << "Initialization is done. Starning...";
-    while (!detail::state::should_close()) {
-        detail::glfw::poll_events();
+	std::vector<std::thread> threads;
+	auto on_add_lambda = [&threads](ui::zone *zone, detail::glfw::window_handle &window) {
+		threads.emplace_back(
+			[zone, &window]() {
+				{
+					clap::ui::detail::glfw::context_guard guard(window);
+					zone->initialize();
+				}
+				clap::log::message::critical << "Window for zone '" << zone->get_name() << "' was initialized.";
 
-        auto iteration_end_point = std::chrono::high_resolution_clock::now() 
-            + std::chrono::milliseconds(size_t(1000.f / 30));
+				auto last_iteration_time_point = std::chrono::high_resolution_clock::now();
+				while (!window.should_close()) {
+					auto current_time_point = std::chrono::high_resolution_clock::now();
+					auto iteration_end_point = current_time_point + iteration_interval;
+					auto time_step = current_time_point - last_iteration_time_point;
+					last_iteration_time_point = current_time_point;
+					{
+						clap::ui::detail::glfw::context_guard guard(window);
+						if (zone->draw(time_step))
+							window.swap_buffers();
+					}
+					std::this_thread::sleep_until(iteration_end_point);
+				}
 
-        //Loop body
+				{
+					clap::ui::detail::glfw::context_guard guard(window);
+					zone->clean_up();
+				}
 
+				clap::log::message::critical << "Window for zone '" << zone->get_name() << "' was cleaned up.";
+				clap::ui::detail::state::remove(zone);
+			}
+		);
+	};
 
-        std::this_thread::sleep_until(iteration_end_point);
-    }
-    log::message::major << "The loop is over. Cleaning up...";
+	while (!detail::state::should_close()) {
+		detail::state::update(on_add_lambda);
+		detail::glfw::wait_events();
+	}
 
-    //Clean up
-    detail::glfw::terminate();
+	for (auto &thread : threads)
+		thread.join();
+	threads.clear();
 
-    return 0;
+	detail::glfw::terminate();
+
+	return 0;
 }
