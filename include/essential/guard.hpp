b5755cc0598a3ce0cc3ca26c528ba3a002b5ea2a
cvelth <cvelth.mail@gmail.com>
2020-09-12 11:20:17 +0300

    Add 'clap::essential::guard'.

---
+++ include/essential/guard.hpp
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+namespace clap::essential {
+	template <typename contained_t, typename lock_lambda_t, typename unlock_lambda_t>
+	class guard {
+	public:
+		guard(contained_t const &contained,
+			  lock_lambda_t const &lock_lambda,
+			  unlock_lambda_t const &unlock_lambda) :
+			contained(contained), lock_lambda(lock_lambda), unlock_lambda(unlock_lambda) {
+			lock_lambda(contained);
+		}
+		~guard() {
+			unlock_lambda(contained);
+		}
+
+		guard(guard const &) = delete;
+		guard(guard &&) = delete;
+		guard& operator=(guard const &) = delete;
+		guard& operator=(guard &&) = delete;
+	private:
+		contained_t const &contained;
+		lock_lambda_t const &lock_lambda;
+		unlock_lambda_t const &unlock_lambda;
+	};
+}
