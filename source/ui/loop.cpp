4b01ccbe1632c1ca8e0029452ca22f51aed2d73d
cvelth <cvelth.mail@gmail.com>
2020-09-12 15:34:06 +0300

    Implement basic 'clap::ui::loop()' structure.

--- source/ui/loop.cpp
+++ source/ui/loop.cpp
@@ -1,12 +1,35 @@
 ﻿#include "ui/loop.hpp"
 #include "ui/detail/glfw.hpp"
+#include "ui/detail/state.hpp"
+
+#include <chrono>
+#include <thread>
+
+#include "nowide/args.hpp"
+
+#include "essential/log.hpp"
 
 int clap::ui::loop(int argc, char **argv) {
-    //Initialization
+    nowide::args _(argc, argv);
+    log::message::minor << "Passed arguments (" << argc << "):";
+    for (int i = 0; i < argc; i++)
+        log::info::critical << i << ": " << argv[i] << ".";
 
-    while (true) {
+    //Initialization
+    
+    log::message::major << "Initialization is done. Starning...";
+    while (!detail::state::should_close()) {
+        detail::glfw::poll_events();
+
+        auto iteration_end_point = std::chrono::high_resolution_clock::now() 
+            + std::chrono::milliseconds(size_t(1000.f / 30));
+
         //Loop body
+
+
+        std::this_thread::sleep_until(iteration_end_point);
     }
+    log::message::major << "The loop is over. Cleaning up...";
 
     //Clean up
     detail::glfw::terminate();
