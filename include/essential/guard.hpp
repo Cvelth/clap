643ddae65c2710768275aa595ce61a350b97db22
cvelth <cvelth.mail@gmail.com>
2020-09-12 20:36:12 +0300

    Redesign 'essential::guard' to lock a mutex (it now inherits 'std::lock_guard').

--- include/essential/guard.hpp
+++ include/essential/guard.hpp
@@ -1,26 +1,25 @@
 ﻿#pragma once
+#include <mutex>
 
 namespace clap::essential {
-	template <typename contained_t, typename lock_lambda_t, typename unlock_lambda_t>
-	class guard {
+	template <typename mutex_t, typename lock_lambda_t, typename unlock_lambda_t>
+	class guard : std::lock_guard<mutex_t> {
 	public:
-		guard(contained_t const &contained,
-			  lock_lambda_t const &lock_lambda,
-			  unlock_lambda_t const &unlock_lambda) :
-			contained(contained), lock_lambda(lock_lambda), unlock_lambda(unlock_lambda) {
-			lock_lambda(contained);
+		guard(mutex_t &mutex,
+			  lock_lambda_t lock_lambda,
+			  unlock_lambda_t unlock_lambda) :
+			std::lock_guard<mutex_t>(mutex), unlock_lambda(unlock_lambda) {
+			lock_lambda();
 		}
 		~guard() {
-			unlock_lambda(contained);
+			unlock_lambda();
 		}
 
 		guard(guard const &) = delete;
 		guard(guard &&) = delete;
-		guard& operator=(guard const &) = delete;
-		guard& operator=(guard &&) = delete;
+		guard &operator=(guard const &) = delete;
+		guard &operator=(guard &&) = delete;
 	private:
-		contained_t const &contained;
-		lock_lambda_t const &lock_lambda;
-		unlock_lambda_t const &unlock_lambda;
+		unlock_lambda_t unlock_lambda;
 	};
 }
