1e6e45c444544b6736fd75d2c8a328598156e4d2
cvelth <cvelth.mail@gmail.com>
2020-09-15 16:42:52 +0300

    Add 'mutex'-less 'simple_guard' version.

--- include/essential/guard.hpp
+++ include/essential/guard.hpp
@@ -2,24 +2,31 @@
 #include <mutex>
 
 namespace clap::essential {
-	template <typename mutex_t, typename lock_lambda_t, typename unlock_lambda_t>
-	class guard : std::lock_guard<mutex_t> {
+	template <typename lock_lambda_t, typename unlock_lambda_t>
+	class simple_guard {
 	public:
-		guard(mutex_t &mutex,
-			  lock_lambda_t lock_lambda,
-			  unlock_lambda_t unlock_lambda) :
-			std::lock_guard<mutex_t>(mutex), unlock_lambda(unlock_lambda) {
+		simple_guard(lock_lambda_t lock_lambda, unlock_lambda_t unlock_lambda) : unlock_lambda(unlock_lambda) {
 			lock_lambda();
 		}
-		~guard() {
+		~simple_guard() {
 			unlock_lambda();
 		}
 
-		guard(guard const &) = delete;
-		guard(guard &&) = delete;
-		guard &operator=(guard const &) = delete;
-		guard &operator=(guard &&) = delete;
+		simple_guard(simple_guard const &) = delete;
+		simple_guard(simple_guard &&) = delete;
+		simple_guard &operator=(simple_guard const &) = delete;
+		simple_guard &operator=(simple_guard &&) = delete;
 	private:
 		unlock_lambda_t unlock_lambda;
+	};
+
+	template <typename mutex_t, typename lock_lambda_t, typename unlock_lambda_t>
+	class guard : public simple_guard<lock_lambda_t, unlock_lambda_t>, protected std::lock_guard<mutex_t> {
+	public:
+		guard(mutex_t &mutex, lock_lambda_t lock_lambda, unlock_lambda_t unlock_lambda)
+			: simple_guard<lock_lambda_t, unlock_lambda_t>(std::forward<lock_lambda_t>(lock_lambda),
+														   std::forward<unlock_lambda_t>(unlock_lambda)), 
+			std::lock_guard<mutex_t>(mutex) {}
+		~guard() {}
 	};
 }
