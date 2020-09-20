33552bc245e5dbb59fe0b44c18ec6c1a4677213f
cvelth <cvelth.mail@gmail.com>
2020-09-20 14:42:58 +0300

    Add 'object_interface' to 'gl'. This class is to be used as a base for all the 'gl' objects which are to interract with the context.

---
+++ include/gl/interface.hpp
@@ -0,0 +1,12 @@
+﻿#pragma once
+namespace clap::gl {
+	namespace detail {
+		class context;
+	}
+	class object_interface {
+	public:
+		object_interface();
+	protected:
+		detail::context * const context;
+	};
+}
---
+++ source/gl/interface.cpp
@@ -0,0 +1,9 @@
+﻿#include "gl/interface.hpp"
+#include "gl/detail/context.hpp"
+
+#include "essential/log.hpp"
+
+clap::gl::object_interface::object_interface() : context(detail::context::current()) {
+	if (!context)
+		clap::log::warning::critical << "Atempting to create an OpenGL object without a current context.";
+}
