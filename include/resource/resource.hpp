3c2ae083713dbd78f0a35854b14e8ae543a5ee8a
Cvelth <cvelth.mail@gmail.com>
2020-12-29 20:35:42 +0200

    Implement shader resource identification The interface is extemely similar to the original implementation except the 'resource' module only provides paths and does not do resource loading leaving it to the caller to figure out.

--- include/resource/resource.hpp
+++ include/resource/resource.hpp
@@ -9,3 +9,37 @@ namespace clap::resource {
 	void wait();
 	bool were_identified();
 }
+
+namespace std::filesystem {
+	class directory_entry;
+}
+namespace clap::resource {
+	namespace detail {
+		template <typename tag_t>
+		class storage {
+		public:
+			std::filesystem::directory_entry *get(std::string_view const &identificator);
+			std::filesystem::directory_entry *try_get(std::string_view const &identificator);
+			inline decltype(auto) operator[](std::string_view const &identificator) {
+				return get(identificator);
+			}
+		};
+
+		class vertex; class fragment;
+		class tesselation_control;
+		class tesselation_evaluation;
+		class geometry; class compute;
+
+		class unknown;
+	}
+
+	namespace shader {
+		extern detail::storage<detail::vertex> vertex;
+		extern detail::storage<detail::fragment> fragment;
+		extern detail::storage<detail::tesselation_control> tesselation_control;
+		extern detail::storage<detail::tesselation_evaluation> tesselation_evaluation;
+		extern detail::storage<detail::geometry> geometry;
+		extern detail::storage<detail::compute> compute;
+	}
+	extern detail::storage<detail::unknown> unknown;
+}
--- source/resource/resource.cpp
+++ source/resource/resource.cpp
@@ -13,6 +13,7 @@ using namespace clap::literals;
 #include <queue>
 #include <shared_mutex>
 #include <thread>
+#include <unordered_map>
 
 [[maybe_unused]] static std::string_view to_std(c4::csubstr const &input) { 
 	return std::string_view{ input.data(), input.size() }; 
@@ -203,34 +204,129 @@ void load_configuration_file(std::filesystem::directory_entry directory_entry,
 	}
 }
 
+bool is_configuration_file(std::filesystem::path const &path) {
+	return clap::configuration::default_configuration_filenames->contains(path.filename().string())
+		|| clap::configuration::additional_configuration_filenames->contains(path.filename().string());
+}
 bool is_ignored(std::filesystem::path const &path) {
 	return clap::configuration::default_ignored_resource_filenames->contains(path.filename().string())
 		|| clap::configuration::ignored_resource_filenames->contains(path.filename().string());
 }
-bool is_configuration_file(std::filesystem::path const &path) {
-	[[maybe_unused]] auto target = path.filename().string();
-	return clap::configuration::default_configuration_filenames->contains(path.filename().string())
-		|| clap::configuration::additional_configuration_filenames->contains(path.filename().string());
+
+struct identified_resource_container_comparator_t : std::equal_to<> {
+	using is_transparent = void;
+};
+struct identified_resource_container_hash_t {
+	using is_transparent = identified_resource_container_comparator_t::is_transparent;
+	using transparent_key_equal = identified_resource_container_comparator_t;
+	size_t operator()(std::string_view txt) const { return std::hash<std::string_view>{}(txt); }
+	size_t operator()(std::string const &txt) const { return std::hash<std::string_view>{}(txt); }
+	size_t operator()(char const *txt) const { return std::hash<std::string_view>{}(txt); }
+};
+using identified_resource_container_t = std::unordered_map<std::string, std::filesystem::directory_entry,
+														   identified_resource_container_hash_t,
+														   identified_resource_container_comparator_t>;
+namespace identified {
+	namespace shader {
+		static identified_resource_container_t vertex;
+		static identified_resource_container_t fragment;
+		static identified_resource_container_t tesselation_control;
+		static identified_resource_container_t tesselation_evaluation;
+		static identified_resource_container_t geometry;
+		static identified_resource_container_t compute;
+	}
+	static identified_resource_container_t unknown;
 }
-void identify(std::filesystem::directory_entry directory_entry,
+namespace clap::resource {
+	namespace shader {
+		detail::storage<detail::vertex> vertex;
+		detail::storage<detail::fragment> fragment;
+		detail::storage<detail::tesselation_control> tesselation_control;
+		detail::storage<detail::tesselation_evaluation> tesselation_evaluation;
+		detail::storage<detail::geometry> geometry;
+		detail::storage<detail::compute> compute;
+	}
+	detail::storage<detail::unknown> unknown;
+}
+
+void add_resource(std::string &&identifier, std::filesystem::directory_entry const &entry,
+				  identified_resource_container_t &container) {
+	auto &&[iterator, success] = container.try_emplace(std::move(identifier), entry);
+	if (!success) 
+		clap::log << cL::warning << cL::major << "clap"_tag << "resource"_tag << "shader"_tag << "identify"_tag
+			<< "Ignore '" << std::filesystem::absolute(entry) << "'.\n" << cL::extra
+			<< "'{resource_map}::try_emplace(...)' has returned 'false'";
+}
+
+identified_resource_container_t *filename_to_container(std::filesystem::path filename) {
+	if (filename == "vertex")
+		return &identified::shader::vertex;
+	if (filename == "fragment")
+		return &identified::shader::fragment;
+	if (filename == "tesscontrol" || filename == "tesselation_control")
+		return &identified::shader::tesselation_control;
+	if (filename == "tesseval" || filename == "tesselation_evaluation")
+		return &identified::shader::tesselation_evaluation;
+	if (filename == "geometry")
+		return &identified::shader::geometry;
+	if (filename == "compute")
+		return &identified::shader::compute;
+	return nullptr;
+}
+void identify_shaders(std::filesystem::directory_entry const &resource_location) {
+	for (auto &directory : std::filesystem::directory_iterator(resource_location)) {
+		auto *container = filename_to_container(directory.path().filename());
+		if (directory.is_directory() && container) {
+			for (auto &entry : std::filesystem::recursive_directory_iterator(directory))
+				if (!entry.is_directory() && !is_ignored(entry))
+					add_resource(entry.path().lexically_relative(directory).replace_extension().string(), 
+								 entry, *container);
+		} else
+			clap::log << cL::warning << cL::serious << "clap"_tag << "resource"_tag << "shader"_tag << "identify"_tag
+				<< "Ignore '" << std::filesystem::absolute(directory) << "'.\n"
+				<< "The name of 'resource/shader/*' directory has to signify the type of shaders it holds."
+				<< "\nSupported directory names are:"
+				<< "\n    - 'vertex'"
+				<< "\n    - 'fragment'"
+				<< "\n    - 'tesscontrol' or 'tesselation_control'"
+				<< "\n    - 'tesseval' or 'tesselation_evaluation'"
+				<< "\n    - 'geometry'"
+				<< "\n    - 'compute'";
+	}
+}
+void identify_unknown(std::filesystem::directory_entry const &resource_location) {
+	for (auto &entry : std::filesystem::recursive_directory_iterator(resource_location))
+		if (!entry.is_directory() && !is_ignored(entry))
+			add_resource(entry.path().lexically_relative(resource_location).replace_extension().string(),
+						 entry, identified::unknown);
+}
+
+void identify(std::filesystem::directory_entry const &directory_entry,
 			  std::queue<std::filesystem::path> &left_to_identify) {
 	if (!is_ignored(directory_entry.path())) {
 		if (directory_entry.is_directory())
-			clap::log << cL::warning << cL::minor << "clap"_tag << "resource"_tag << "identify"_tag
-				<< "limited"_tag << "unfinished"_tag << "to_do"_tag
-				<< "Ignore a resource directory: '"
-				<< std::filesystem::absolute(directory_entry.path()) << "'.\n"
-				<< "Configuration files are the only supported resource type "
-					"and they must be put in the root of 'resource' directory.\n"
-				<< "More resource type support is to be added in future iterations.";
+			if (directory_entry.path().filename() == "shader")
+				identify_shaders(directory_entry);
+			else
+				identify_unknown(directory_entry);
 		else if (is_configuration_file(directory_entry.path()))
 			load_configuration_file(directory_entry, left_to_identify);
 		else
-			clap::log << cL::warning << cL::major << "clap"_tag << "resource"_tag << "identify"_tag
-				<< "Ignore a resource: '"
-				<< std::filesystem::absolute(directory_entry.path()) << "'.\n"
-				<< cL::extra << "Resource identification failed. "
-					"The resource is either unsupported or broken.";
+			clap::log << cL::warning << cL::minor << "clap"_tag << "resource"_tag << "identify"_tag
+			<< "Ignore a resource: '"
+			<< std::filesystem::absolute(directory_entry.path()) << "'.\n"
+			<< "Configuration files are the only resource type supported "
+				"in the root of 'resource' directory.";
+	}
+}
+
+void log(clap::logger::detail::logger_stream_t &logger_stream, std::string_view name, 
+		 identified_resource_container_t const &container) {
+	if (!container.empty()) {
+		logger_stream << cL::important << "    " << name << " (" << container.size() << ")" 
+			<< cL::minor << "        ";
+		for (auto &entry : container)
+			logger_stream << entry.first << "  ";
 	}
 }
 
@@ -265,6 +361,18 @@ void clap::resource::identify() {
 					left_to_identify.pop();
 				}
 
+				{
+					auto logger_stream = clap::log.add_entry();
+					logger_stream << cL::message << cL::important << "clap"_tag << "resource"_tag << "identify"_tag
+						<< "Complete resource identification.\n";
+					::log(logger_stream, "Vertex Shaders", identified::shader::vertex);
+					::log(logger_stream, "Fragment Shaders", identified::shader::fragment);
+					::log(logger_stream, "Tesselation Control Shaders", identified::shader::tesselation_control);
+					::log(logger_stream, "Tesselation Evaluation Shaders", identified::shader::tesselation_evaluation);
+					::log(logger_stream, "Geometry Shaders", identified::shader::geometry);
+					::log(logger_stream, "Compute Shaders", identified::shader::compute);
+					::log(logger_stream, "Unknown Resources", identified::unknown);
+				}
 				identification_status = true;
 			}
 		).detach();
@@ -274,4 +382,71 @@ void clap::resource::wait() {
 }
 bool clap::resource::were_identified() {
 	return identification_status;
+}
+
+template <typename tag_t>
+identified_resource_container_t &get_container();
+template<> inline identified_resource_container_t &get_container<clap::resource::detail::vertex>() { 
+	return identified::shader::vertex; 
+}
+template<> inline identified_resource_container_t &get_container<clap::resource::detail::fragment>() {
+	return identified::shader::fragment;
+}
+template<> inline identified_resource_container_t &get_container<clap::resource::detail::tesselation_control>() {
+	return identified::shader::tesselation_control;
+}
+template<> inline identified_resource_container_t &get_container<clap::resource::detail::tesselation_evaluation>() {
+	return identified::shader::tesselation_evaluation;
+}
+template<> inline identified_resource_container_t &get_container<clap::resource::detail::geometry>() {
+	return identified::shader::geometry;
+}
+template<> inline identified_resource_container_t &get_container<clap::resource::detail::compute>() {
+	return identified::shader::compute;
+}
+template<> inline identified_resource_container_t &get_container<clap::resource::detail::unknown>() {
+	return identified::unknown;
+}
+
+template<typename tag_t> std::filesystem::directory_entry *get_impl(std::string_view const &identifier) {
+	auto &container = get_container<tag_t>();
+	if (auto iterator = container.find(identifier); iterator != container.end())
+		return &iterator->second;
+	else
+		clap::log << cL::warning << cL::serious << "clap"_tag << "resource"_tag << "get"_tag
+			<< "Fail to provide a resource: " << identifier << ". Make sure it was identified.";
+	return nullptr;
+}
+template<typename tag_t> std::filesystem::directory_entry *
+clap::resource::detail::storage<tag_t>::get(std::string_view const &identifier) {
+	std::shared_lock guard(identification_mutex);
+	return get_impl<tag_t>(identifier);
+}
+template<typename tag_t> std::filesystem::directory_entry *
+clap::resource::detail::storage<tag_t>::try_get(std::string_view const &identifier) {
+	if (identification_mutex.try_lock_shared()) {
+		std::shared_lock guard(identification_mutex, std::adopt_lock);
+		return get_impl<tag_t>(identifier);
+	} else
+		clap::log << cL::warning << cL::minor << "clap"_tag << "resource"_tag << "try_get"_tag
+			<< "Fail to lock 'storage<...>::try_get(...)' mutex. Resource manager is already in use.";
+	return nullptr;
+}
+
+namespace clap::resource::detail {
+	using namespace std::filesystem;
+	template directory_entry *storage<vertex>::get(std::string_view const &identifier);
+	template directory_entry *storage<fragment>::get(std::string_view const &identifier);
+	template directory_entry *storage<tesselation_control>::get(std::string_view const &identifier);
+	template directory_entry *storage<tesselation_evaluation>::get(std::string_view const &identifier);
+	template directory_entry *storage<geometry>::get(std::string_view const &identifier);
+	template directory_entry *storage<compute>::get(std::string_view const &identifier);
+	template directory_entry *storage<unknown>::get(std::string_view const &identifier);
+	template directory_entry *storage<vertex>::try_get(std::string_view const &identifier);
+	template directory_entry *storage<fragment>::try_get(std::string_view const &identifier);
+	template directory_entry *storage<tesselation_control>::try_get(std::string_view const &identifier);
+	template directory_entry *storage<tesselation_evaluation>::try_get(std::string_view const &identifier);
+	template directory_entry *storage<geometry>::try_get(std::string_view const &identifier);
+	template directory_entry *storage<compute>::try_get(std::string_view const &identifier);
+	template directory_entry *storage<unknown>::try_get(std::string_view const &identifier);
 }
