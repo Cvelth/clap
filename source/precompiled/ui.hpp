980c8311e65b70ae668e70065dd3f5b007621d5f
Cvelth <cvelth.mail@gmail.com>
2020-12-30 12:03:22 +0200

    Move(and rename) 'ui/detail/vulkan.*' to 'ui/vulkan/core.*'

--- source/precompiled/ui.hpp
+++ source/precompiled/ui.hpp
@@ -7,13 +7,14 @@
 #include "essential/utility.hpp"
 #include "resource/resource.hpp"
 #include "ui/detail/manager.hpp"
-#include "ui/detail/vulkan.hpp"
+#include "ui/vulkan/core.hpp"
 #include "ui/compound.hpp"
 #include "ui/loop.hpp"
 #include "ui/zone.hpp"
 using namespace clap::literals;
 
 #include <algorithm>
+#include <array>
 #include <concepts>
 #include <functional>
 #include <iterator>
